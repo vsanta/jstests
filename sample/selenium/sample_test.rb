84988141c5a5a4810cff0e7e9f12f7f36498d384
Vanessa Sant Anna <vanessa.santanna@gmail.com>
2011-03-13 22:04:50 -0300

    Adding a template for a Selenium test to make it easier to add js testing to a CI server

---
+++ sample/selenium/sample_test.rb
@@ -0,0 +1,36 @@
+require "test/unit"
+require "rubygems"
+gem "selenium-client"
+require "selenium/client"
+require "ruby-debug"
+
+class SampleSeleniumTest < Test::Unit::TestCase
+
+  def setup
+    @verification_errors = []
+    @selenium = Selenium::Client::Driver.new \
+      :host => "localhost",
+      :port => 4444,
+      :browser => "*firefox",
+      :url => "http://localhost:8000/",
+      :timeout_in_second => 60,
+      :profile => "selenium"
+
+    @selenium.start_new_browser_session
+  end
+  
+  def teardown
+    @selenium.close_current_browser_session
+    assert_equal [], @verification_errors
+  end
+  
+  def assert_all_qunit_pass timeout=10
+    @selenium.wait_for_condition 'selenium.browserbot.getCurrentWindow().QUnit.suite_done', timeout
+    assert_equal "0", @selenium.get_text("//p[@id='qunit-testresult']/span[@class=\"failed\"]")
+  end
+  
+  def test_selenium
+    @selenium.open "/sample/sample-suite.html"
+    assert_all_qunit_pass
+  end
+end
